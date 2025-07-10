feb505bdefd86cf6faf468631930596b85ba166e
vaibhav1445 <144265525+vaibhav1445@users.noreply.github.com>
2025-07-10 15:55:30 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -68,4 +68,7 @@ socket.on("transferComplete", ({ roomId, fileName }) => {
 });
 
 const PORT = process.env.PORT || 4000;
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
